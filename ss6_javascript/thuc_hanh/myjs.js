825d6a3b5cbdfcee5376891d15ea95e9a4fb0f96
Vinhnd03 <ducvinhmycanh@gmail.com>
2025-03-07 14:52:51 +0700

    nop bai tap ss6

---
+++ ss6_javascript/thuc_hanh/myjs.js
@@ -0,0 +1,16 @@
+function showAlert(){
+    alert("đây là alert()!");
+}
+
+function showDocumentWrite(){
+    document.write("đây là document.write()");
+}
+
+
+function showConsoleLog(){
+    console.log("đây là console.log()");
+}
+
+function showMessage(){
+    document.getElementById('message').innerHTML = "xin chao";
+}
